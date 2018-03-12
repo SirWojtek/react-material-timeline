3c2a634ab0c6791e58a69b2246045dc55dcbaebe
Mateusz <mat@citrusad.com>
2018-03-12 12:44:52 +1000

    working example

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,9 +2,10 @@ import { render } from 'react-dom';
 import * as React from 'react';
 
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import { withStyles, WithStyles, StyleRulesCallback } from 'material-ui/styles';
+import { Avatar, Icon } from 'material-ui';
 import { blue, pink } from 'material-ui/colors';
-import { Grid } from 'material-ui';
+
+import Timeline, { IEvent } from '../src/timeline/timeline';
 
 const theme = createMuiTheme({
   palette: {
@@ -13,11 +14,37 @@ const theme = createMuiTheme({
   }
 });
 
+const icon =
+  <Avatar>
+    <Icon>work</Icon>
+  </Avatar>;
+
+const events: IEvent[] = [
+  {
+    time: new Date(),
+    title: 'Event 1',
+    description: [ 'Some description for event 1' ],
+    icon
+  },
+  {
+    time: new Date(),
+    title: 'Event 2',
+    description: [ 'Some description for event 2' ],
+    icon
+  },
+  {
+    time: new Date(),
+    title: 'Event 3',
+    description: [ 'Some description for event 3' ],
+    icon
+  }
+];
+
 class Demo extends React.Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-		test
+	<Timeline events={events}/>
       </MuiThemeProvider>
     );
   }
