19daa46ca3e87cd9788fd1871825baa5033b6a5b
Mateusz <mat@citrusad.com>
2018-03-28 20:01:00 +1000

    umd deployment configuration

---
+++ webpack.config.deploy.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    'react-material-timeline': './src/index.ts',
+    'react-material-timeline.min': './src/index.ts',
+  },
+  output: {
+    path: path.resolve('deploy/_bundles'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'react-material-timeline',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    modules: [ 'src', 'node_modules'],
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.deploy.json'
+        }
+      },
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true,
+      include: /\.min\.js$/,
+    })
+  ],
+};
